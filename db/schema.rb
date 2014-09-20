8d3cf8504712fe8b36e708ed8fc6c2c3f51fec2e
Bryan Collazo <bcollazo@mit.edu>
2014-09-20 03:36:42 -0400

    adsf

--- db/schema.rb
+++ db/schema.rb
@@ -11,22 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140920030851) do
-
-  create_table "libraries", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "ques", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "rooms", force: true do |t|
-    t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
